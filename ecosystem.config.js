9a188294ac4feee92cdcf80e9fab6cc087bfa401
Zim <zimtsui@gmail.com>
2019-10-21 00:14:28 +0800

    dev

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+const { basename } = require('path');
+
+module.exports = {
+    apps: [{
+        name: basename(__dirname),
+        script: '.',
+        cwd: __dirname,
+        listen_timeout: 10000,
+        kill_timeout: 10000, // 随便设个比两个环境变量之和大的数
+        env: {
+            NODE_ENV: 'production',
+        },
+    }],
+};
